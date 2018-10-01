7f7ecb1a2c0e8fbf7511df375a7339f8be468046
sfilkov21 <43067397+sfilkov21@users.noreply.github.com>
2018-10-01 08:32:41 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,26 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Data type then variable name to declare a variable
+		number = 20;
+		System.out.println("number: " + number);
+		number += 5; // number = number + 5
+		System.out.println("Add 5:" + number);
+		number /= 2; //number = number /2
+		System.out.println("Divide by 2:" + number); 
+		number *= 4; //number = number *4
+		System.out.println("Multiply by 4: ");
+		System.out.println(number);
+		number -= 6; //number = number -6
+		System.out.println("Subtract by 6: " + number);
+		number %= 2; //number = number %2
+		System.out.println("Mod 2: " + number);
+		
+		
+		
+	}
+
+}
